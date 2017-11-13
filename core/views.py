0e98df8d294a5fc408610e00d555ea3bb3b7a104
janariafurtado <janaria.furtado@yahoo.com.br>
2017-11-13 16:53:38 -0200

    alteração

--- core/views.py
+++ core/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from app.models import *
+from datetime import datetime
 
-# Create your views here.
 
-def index(request):
-    context = {
-        'title':'Faculdade Inovação Tecnologia'
-    }
-    return render(request, 'index.html', context)
+def home(request):
+	assert isinstance(request, HttpRequest)
+	return render(
+		request, 
+		'index.html',
+        context_instance = RequestContext(request,
+        {
+            'year':datetime.now().year,
+        })
+    )
+    
 
--- site1/urls.py
+++ site1/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, url
+from datetime import datetime
 from django.contrib import admin
 from core.views import *
 
 urlpatterns = [
-    url(r'^$', index),
+    url(r'^$', 'core.wews.home', name='home'),
     url(r'^admin/', admin.site.urls),
 ]
