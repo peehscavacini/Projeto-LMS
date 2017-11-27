1047c33aa080feb229dd2b63199ea57e771859df
brunobuzzo <30813663+brunobuzzo@users.noreply.github.com>
2017-11-27 02:37:56 -0200

    Update Banco 1 - Criação.sql

--- Script Banco de Dados/Banco 1 - Criação.sql
+++ Script Banco de Dados/Banco 1 - Criação.sql
@@ -42,7 +42,6 @@ CREATE TABLE CURSO (
     id    integer identity(1,1) not null,
     sigla varchar(5) not null,
     nome  varchar(50) not null,
-	coordenador_id integer not null,
 	CONSTRAINT pk_curso PRIMARY KEY (id),
     CONSTRAINT un_curso_sigla UNIQUE (sigla),
     CONSTRAINT un_curso_nome UNIQUE (nome)
